33239ef4c742f880d4b3b6a7aa6e58121bb53d8c
rivaldoanugerah <72857970+rivaldoanugerah@users.noreply.github.com>
2020-10-27 22:52:48 +0700

    Upload Perubahan Project Wordpress Challenge 2
    Upload Perubahan Web  Challenge 2 Jong Koding Rivaldo Anugerah

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1604388480;s:7:"authKey";s:64:"~4z?Bt>.mF+w:04/4F;++c%/)@h_Rek+t;cV+GXg/Gc1NbUKG<<li4,#W2X2]ZjS";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;}
